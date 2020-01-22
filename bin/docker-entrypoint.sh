e3ed011576c0c7414005f29cb63b682d7d6a7c4f
Claudio Canales <ccanales@progrexion.com>
2020-01-22 11:00:41 -0700

    Add MySQL and sleeps to avoid bugs

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -12,21 +12,26 @@ echo "DO account is authorized in the container"
 # init_k10 script
 
 if [ $1 == "init_k10" ]; then
-    echo "Starting function init_k10"
+    echo "==========Starting function init_k10=========="
     kubectl create namespace kasten-io
     helm repo add kasten https://charts.kasten.io/
     helm install k10 kasten/k10 --namespace=kasten-io
+    echo "==========Waiting to install mysql chart=========="
+    sleep 10
     kubectl annotate --overwrite volumesnapshotclass do-block-storage k10.kasten.io/is-snapshot-class=true
+    helm repo add stable https://kubernetes-charts.storage.googleapis.com/
     helm install my-release stable/mysql
 fi
 
 if [ $1 == "clean_k10" ]; then
-    echo "Starting function clean_k10"
+    echo "==========Starting function clean_k10=========="
     helm uninstall my-release
     kubectl annotate --overwrite volumesnapshotclass do-block-storage k10.kasten.io/is-snapshot-class=false
     helm uninstall k10 -n kasten-io
+    echo "==========Waiting to delete the ns=========="
     sleep 20
     kubectl delete namespace kasten-io
 fi
 
+echo "==========function init_k10 COMPLETED=========="
 sleep 1000
